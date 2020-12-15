6caa9e1023926f01f324ec768df4fc0b582fc6fd
JR Nova <novakoda9@gmail.com>
2020-12-15 15:28:13 -0600

    switched NameForm to functional component

--- src/components/NameForm.js
+++ src/components/NameForm.js
@@ -1,59 +1,53 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import data from '../data';
 
-class NameForm extends Component  {
-  constructor(props) {
-    super(props);
-    this.state = {
-      firstName: data.name.first,
-      lastName: data.name.last,
-    };
-    this.updateState = this.updateState.bind(this);
-    this.changeName = this.changeName.bind(this);
-  }
+const NameForm = props => {
+  const [name, setName] = useState({
+    firstName: data.name.first,
+    lastName: data.name.last
+  });
 
-  changeName(e) {
+  const changeName = e => {
     e.preventDefault();
     // console.log('pre ' + data.name.first, data.name.last + ' | ', this.state.firstName, this.state.lastName);
-    data.name.first = this.state.firstName;
-    data.name.last = this.state.lastName;
-    this.props.done();
+    data.name.first = name.firstName;
+    data.name.last = name.lastName;
+    props.done();
     // console.log('post ' + data.name.first, data.name.last + ' | ', this.state.firstName, this.state.lastName);
   }
 
-  updateState(e) {
-    this.setState({ [e.target.id]: e.target.value });
+  const updateName = e => {
+    setName({ ...name, [e.target.id]: e.target.value });
   }
 
-  render() {
-    return (
-      <form onSubmit={this.changeName}>
-        <div class="form-group">
-          <label>First Name
-            <input
-              type="text"
-              id="firstName"
-              value={this.state.firstName}
-              onChange={this.updateState}
-              className="form-control"
-            ></input>
-          </label>
-        </div>
-        <div class="form-group">
-          <label>Last Name
-            <input
-              type="text"
-              id="lastName"
-              value={this.state.lastName}
-              onChange={this.updateState}
-              className="form-control"
-            ></input>
-          </label>
-        </div>
-        <button className="btn btn-primary mt-1">Submit</button>
-      </form>
-    )
-  }
+  return (
+    <form onSubmit={changeName}>
+      <div className="form-group">
+        <label>First Name
+          <input
+            type="text"
+            id="firstName"
+            value={name.firstName}
+            onChange={updateName}
+            className="form-control"
+          ></input>
+        </label>
+      </div>
+      <div className="form-group">
+        <label>Last Name
+          <input
+            type="text"
+            id="lastName"
+            value={name.lastName}
+            onChange={updateName}
+            className="form-control"
+          ></input>
+        </label>
+      </div>
+      <button className="btn btn-primary mt-1">Submit</button>
+    </form>
+  )
+
 }
 
 export default NameForm;
